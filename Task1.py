1421e70e945faa61a86bf69e5902d7091fb9e209
Rowida <73389900+rowida22@users.noreply.github.com>
2021-12-16 22:50:00 +0200

    linting

--- Task1.py
+++ Task1.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,11 +7,11 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome("C:\\Program Files (x86)\\chromedriver.exe")
 
-driver.get("https://techwithtim.net") 
+driver.get("https://techwithtim.net")
 print(driver.title)
 
 
-search = driver.find_element(By.NAME,"s")
+search = driver.find_element(By.NAME, "s")
 search.send_keys("test")
 search.send_keys(Keys.RETURN)
 
@@ -20,12 +20,13 @@ try:
     EC.presence_of_element_located((By.ID, "main"))
   )
  
-  articals = main.find_elements(By.TAG_NAME,"article")
+  articals = main.find_elements(By.TAG_NAME, "article")
   for articale in articals:
   
-    header = articale.find_element(By.CLASS_NAME ,"entery-summary")
+    header = articale.find_element(By.CLASS_NAME, "entery-summary")
     print(header.text)
       
 finally:
-  driver.quit()
+    driver.quit()
+
 
