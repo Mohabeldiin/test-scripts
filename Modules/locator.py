9dff4fe5f5725a88c20758342d99d5deeae1b7a2
Mohab Mohsen <54236117+Mohabeldiin@users.noreply.github.com>
2021-12-17 18:30:12 +0200

    MO_updating Locator.py_ading Registration locator

--- Modules/locator.py
+++ Modules/locator.py
@@ -1,8 +1,8 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators(object):
-    """this class holds facebook main page locators"""
+class LoginPageLocators(object):
+    """this class holds facebook login page locators"""
     LOGIN_BUTTON = (By.NAME, 'login')
     REGISTRATION_BUTTON = (By.ID, 'u_0_2_Vw')
     EMAIL_TEXTFIELD = (By.NAME,'email')
@@ -11,6 +11,18 @@ class MainPageLocators(object):
 
 
 class RegistrationPageLocators:
-    """foo"""
+    """this class holds facebook Registration page locators"""
     FIRSTNAME_TEXTFIELD = (By.NAME,'firstname')
-    
+    LASTNAME_TEXTFIELD = (By.NAME,'lastname')
+    MOBILENUMBER_TEXTFIELD = (By.NAME,'reg_email__')
+    EMAILADDRESS_TEXTFIELD = (By.NAME,'reg_email__')
+    NEWPASSWORD_TEXTFIELD = (By.NAME,'reg_passwd__')
+    DATEOFBIRTH_DAY_SELECTOR = (By.NAME,'birthday_day')
+    DATEOFBIRTH_MONTH_SELECTOR = (By.NAME,'birthday_month')
+    DATEOFBIRTH_YEAR_SELECTOR = (By.NAME,'birthday_year')
+    GENDER_MALE_RADIO = (By.ID, 'u_9_5_4b')
+    GENDER_FEMALE_RADIO = (By.ID, 'u_9_4_1')
+    GENDER_CUSTOM_RADIO = (By.ID, 'u_9_6_fT')
+    GENDER_CUSTOM_PRONOUN_RADIO = (By.ID, 'js_1x4')
+    GENDER_CUSTOM_OPTINAL_GENDER_TEXTFIELD = (By.NAME,'custom_gender')
+    SIGNUP_BUTTON = (By.NAME, 'websubmit')
