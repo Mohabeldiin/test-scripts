ab69aa9cc391c47b525398b9648e4af3cef2cc52
Mohab Mohsen <54236117+Mohabeldiin@users.noreply.github.com>
2021-12-21 19:17:46 +0200

    adding __all__ to MODULS __init__

--- Modules/__init__.py
+++ Modules/__init__.py
@@ -1,3 +1,11 @@
-    """this file is to fix packaging and modeling in python 
-        for more information read https://docs.python.org/3/tutorial/modules.html#packages"""
-        
+"""this file is to fix packaging and modeling in python 
+    for more information read https://docs.python.org/3/tutorial/modules.html#packages"""
+import locator
+import element
+import page
+
+"""the __all__ contains the moduls names
+    NOTE: not that __all__ must be always updated
+    USE: __all__ used to import all mudols from that package by using import *
+    WHY: __all__ fix packaging error when use import *"""
+__all__ =["page", "element", "locator"]
