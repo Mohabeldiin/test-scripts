0b48da64e1b2f207b16213a4431fd375fa499b6e
Mohab Mohsen <54236117+Mohabeldiin@users.noreply.github.com>
2021-12-25 04:34:49 +0200

    editing locator __init__

--- Modules/locator/__init__.py
+++ Modules/locator/__init__.py
@@ -1,8 +1,6 @@
-'''this module contains all pages locators 
+"""this module contains all pages locators 
         NOW: now it contains facebook pages locators
-        LATER: it will contain the AI module to auto detect page elments'''
-__doc__ = '''this module contains all pages locators 
-        NOW: now it contains facebook pages locators
-        LATER: it will contain the AI module to auto detect page elments'''
-import locator
-__import__ = locator
+        LATER: it will contain the AI module to auto detect page elments"""
+from locator import *
+from locator import RegistrationPageLocators
+from locator import LoginPageLocators
