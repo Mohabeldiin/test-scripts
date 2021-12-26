8d24de9947f3d9a1eb23a4c6201dba3cdc837324
Mohab Mohsen <54236117+Mohabeldiin@users.noreply.github.com>
2021-12-26 21:30:17 +0200

    Test_06_Registration

--- Test-Suite_Registration/test_06_Registration.py
+++ Test-Suite_Registration/test_06_Registration.py
@@ -1,11 +1,18 @@
 """this test case Check the Email text field that has Email validation
     TC_06_Registration from https://www.loginradius.com/blog/async/test-cases-for-registration-and-login-page/"""
+import enum
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+class Test_Data(enum.Enum):
+    """this class is enum holds the test data that is used in this test case"""
+    email_without_at = "testgmail.com"
+    email_random = "test@gmail.com"
+    email_without_at_in_word = "testAtgmail.com"
+    email_without_dot = "test@gmailcom"
 
 class Test_06_Registration(unittest.TestCase):
     """1- Enter Invalid Emails.
@@ -27,26 +34,73 @@ class Test_06_Registration(unittest.TestCase):
         self.driver.implicitly_wait(5)
         self.driver.get('https://www.facebook.com/')
         self.driver.maximize_window()
+        self.login_form_locator = (By.CLASS_NAME,"_9vtf")
+        self.creat_new_account_locator = (By.ID, "u_0_2_Yq")
 
+        try:
+            main = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(self.login_form_locator)
+            )
+            self.creat_new_acount = self.driver.find_element(*self.creat_new_account_locator)
+            self.creat_new_acount.click()
+        except: assert False
+
+        self.email_textfiled_locator = (By.NAME,'reg_email__')
+        self.confirme_email_textfiled_locator = (By.NAME,'reg_email_confirmation__')
 
     def test_01_Email_validation(self):
         """Check the Email text field that has an Email address without @ symbol."""
-        pass
+        try:
+            email = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(self.email_textfiled_locator)
+            )
+            email.clear()
+            email.send_keys(Test_Data.email_without_at)
+            """hna msh hados 3la sinup button le facebook bs
+                han7tag n8iarha b3diin le ba2ii el mwak3"""
+            assert EC.invisibility_of_element_located(self.confirme_email_textfiled_locator)
+        except: assert False
 
 
     def test_02_Email_validation(self):
         """Check the Email text field that has a random string instead of a real email."""
-        pass
+        try:
+            email = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(self.email_textfiled_locator)
+            )
+            email.clear()
+            email.send_keys(Test_Data.email_random)
+            """hna msh hados 3la sinup button le facebook bs
+                han7tag n8iarha b3diin le ba2ii el mwak3"""
+            assert EC.invisibility_of_element_located(self.confirme_email_textfiled_locator)
+        except: assert False
 
 
     def test_03_Email_validation(self):
         """Check the Email text field that has @ symbol written in words."""
-        pass
-
+        try:
+            email = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(self.email_textfiled_locator)
+            )
+            email.clear()
+            email.send_keys(Test_Data.email_without_at_in_word)
+            """hna msh hados 3la sinup button le facebook bs
+                han7tag n8iarha b3diin le ba2ii el mwak3"""
+            assert EC.invisibility_of_element_located(self.confirme_email_textfiled_locator)
+        except: assert False
 
     def test_04_Email_validation(self):
         """Check the Email text field that has a missing dot in the email address."""
-        pass
+        try:
+            email = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(self.email_textfiled_locator)
+            )
+            email.clear()
+            email.send_keys(Test_Data.email_without_dot)
+            """hna msh hados 3la sinup button le facebook bs
+                han7tag n8iarha b3diin le ba2ii el mwak3"""
+            assert EC.invisibility_of_element_located(self.confirme_email_textfiled_locator)
+        except: assert False
 
 
     def tearDown(self):
