f3de7fb349485bfbc52378c9296167d8f435993a
Mohab Mohsen <54236117+Mohabeldiin@users.noreply.github.com>
2021-12-26 17:22:13 +0200

    Revmoeing unused imports

--- Test-Suite_Registration/test_01_Registration.py
+++ Test-Suite_Registration/test_01_Registration.py
@@ -3,7 +3,6 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from Modules.locator.locator import *
