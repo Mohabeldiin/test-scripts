69ac5d8c886af985e9ae144c438018317315c1f5
Mohab Mohsen <54236117+Mohabeldiin@users.noreply.github.com>
2021-12-26 19:54:12 +0200

    fixing Test_02_Registration

--- Test-Suite_Registration/test_02_Registration.py
+++ Test-Suite_Registration/test_02_Registration.py
@@ -5,8 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from Modules.locator.locator import *
-
 
 class Test_02_Registration(unittest.TestCase):
     """1-Do not enter any value in the field.
@@ -20,15 +18,15 @@ class Test_02_Registration(unittest.TestCase):
         self.driver.implicitly_wait(5)
         self.login_form = (By.CLASS_NAME,"_9vtf")
         self.sinup_button = (By.NAME,"websubmit")
-        self.creat_new_account = (By.CLASS_NAME, "_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy")
+        self.creat_new_account = (By.ID, "u_0_2_Yq")
         self.textfiled_mandatory_symbol = (By.CLASS_NAME, "_5dbc img sp_98fCI7IVTTz_1_5x sx_e1ddd6")
         self.radio_mandatory_symbol = (By.CLASS_NAME, "_5dbc _5k_6 img sp_98fCI7IVTTz_1_5x sx_e1ddd6")
         try:
             main = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located(*self.login_form)
+                EC.presence_of_element_located(self.login_form)
             )
-            if EC.presence_of_element_located(*self.login_form) :
-                self.creat_new_acount = self.driver.find_element(*self.creat_new_acount)
+            if EC.presence_of_element_located(self.login_form) :
+                self.creat_new_acount = self.driver.find_element(*self.creat_new_account)
                 self.creat_new_acount.click()
         except: assert False
         
@@ -38,7 +36,7 @@ class Test_02_Registration(unittest.TestCase):
         """Check the required fields by not filling any data"""
         try:#clicking the sinup button
             main = WebDriverWait(self.driver, 10).until(
-                EC.element_to_be_clickable(*self.sinup_button)
+                EC.element_to_be_clickable(self.sinup_button)
             )
             main.click()
             self.driver.find_element(*self.sinup_button).click()
@@ -47,10 +45,10 @@ class Test_02_Registration(unittest.TestCase):
         
         try:#checking the mandatory_symbol
             textfileds_mandatorys = WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_all_elements_located(*self.textfiled_mandatory_symbol)
+                EC.visibility_of_all_elements_located(self.textfiled_mandatory_symbol)
             )
             radios_mandatorys = WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_all_elements_located(*self.radio_mandatory_symbol)
+                EC.visibility_of_all_elements_located(self.radio_mandatory_symbol)
             )
             assert len(textfileds_mandatorys) == 4 & len(radios_mandatorys) == 2
         except: assert False
@@ -60,3 +58,7 @@ class Test_02_Registration(unittest.TestCase):
     def tearDown(self):
         """this method will be called after every test"""
         self.driver.quit()
+
+if __name__ == "__main__":
+    """This is the main function will Run the Unit Test if this Moudle is not imported"""
+    unittest.main()
