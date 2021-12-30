d5946bcd46d24e7bdef9b647c9b2786ff3856d53
rowida22 <rowida91200@gmail.com>
2021-12-30 16:21:23 +0200

    test17

--- Test-Suite_login/test_17_login.py
+++ Test-Suite_login/test_17_login.py
@@ -2,11 +2,13 @@
    Login_17 from https://sampletestcases.com/latest-sample-testcases-of-facebook-login-page/"""
 import enum
 import unittest
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 
 class Test_Data(enum.Enum):
     """Test data for login test case"""
@@ -25,18 +27,18 @@ class Test_17_login(unittest.TestCase):
 
     def test_15(self):
         """To verify that entered multiple times incorrect passwords."""
+        
         try:
-          passwd = WebDriverWait(self.driver, 10).until(
-              EC.presence_of_element_located(self.password_locator)
-            )
-            for i in range(0,3):
-                passwd.send_keys(Test_Data.password.value)
-                passwd.send_keys(Keys.RETURN)
+               
+                for i in range(0,3):
+                    passwd = WebDriverWait(self.driver, 10).until(
+                        EC.presence_of_element_located(self.password_locator)
+                        )
+                    passwd.send_keys(Test_Data.password.value)
+                    passwd.send_keys(Keys.RETURN)
+                    time.sleep(15)
+                    
         except: assert False
-
-
-         
-          
 
     def tearDown(self):
         """this function run after every test"""
