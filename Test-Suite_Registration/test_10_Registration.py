2785bdf69ad2ffed0ff1af68ed11c3fe6487f2cd
Mohab Mohsen <54236117+Mohabeldiin@users.noreply.github.com>
2022-01-04 19:50:39 +0200

    TC_10_Registration test case completed successfully

--- Test-Suite_Registration/test_10_Registration.py
+++ Test-Suite_Registration/test_10_Registration.py
@@ -1,23 +1,24 @@
-"""this test case foo
-    TC_foo_Registration from https://www.loginradius.com/blog/async/test-cases-for-registration-and-login-page/"""
+"""this test case Check the phone number when passing country code
+    TC_10_Registration from https://www.loginradius.com/blog/async/test-cases-for-registration-and-login-page/"""
 
 
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
 class Test_Data(object):
     """this class is enum holds the test data that is used in this test case"""
-    pass
+    PHONE_NUMBER = "+9190112244"
 
 
-class Test_foo_Registration(unittest.TestCase):
-    """foo\n
-    1- foo\n
-    2- foo"""
+class Test_10_Registration(unittest.TestCase):
+    """Check the phone number when passing country code\n
+    1- Enter valid phone number with country code.\n
+    2- Click on Register button."""
 
     def setUp(self):
         """this method will be called before every test"""
@@ -25,11 +26,59 @@ class Test_foo_Registration(unittest.TestCase):
         self.driver.implicitly_wait(5)
         self.driver.get('https://www.facebook.com/')
         self.driver.maximize_window()
+        self.login_form_locator = (By.CLASS_NAME,"_9vtf")
+        self.creat_new_account_locator = (By.LINK_TEXT, "Create New Account")
 
-    def test_foo_foo(self):
-        """foo\n
-        EC: foo"""
-        pass
+        try:
+            if WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(self.login_form_locator)
+            ).is_displayed():
+                self.creat_new_acount = WebDriverWait(self.driver, 10).until(
+                    EC.element_to_be_clickable(self.creat_new_account_locator)
+                )
+                self.creat_new_acount.click()
+        except TimeoutException:
+            print("\n###############\n",TimeoutException.__doc__ , "\n###############\n")
+            assert False
+
+        self.phone_textfiled_locator = (By.NAME,'reg_email__')
+        self.sinUp_button_locator = (By.NAME, "websubmit")
+        self.error_message_locator = (By.ID, "reg_error_inner")
+
+    def test_01_Phone_Number_validation(self):
+        """Check the phone number when passing country code\n
+        EC: It should not show any validation message"""
+        try:
+            try:
+                phone = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located(self.phone_textfiled_locator)
+                )
+                phone.send_keys(Test_Data.PHONE_NUMBER)
+            except AssertionError:
+                print("\n###############\n",AssertionError.__doc__ , "\n###############\n")
+                assert False
+            try:
+                sinup = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located(self.sinUp_button_locator)
+                )
+                sinup.click()
+            except AssertionError:
+                print("\n###############\n",AssertionError.__doc__ , "\n###############\n")
+                assert False
+            try:
+                if WebDriverWait(self.driver, 10).until(
+                    EC.invisibility_of_element_located(self.error_message_locator)
+                ):
+                    assert True
+                else:
+                    print("EC != AC")
+                    assert False
+            except AssertionError:
+                print("\n###############\n",AssertionError.__doc__ , "\n###############\n")
+                assert False
+        except AssertionError:
+            print("\n###############\n",AssertionError.__doc__ , "\n###############\n")
+            assert False
 
     def tearDown(self):
         """this method will be called after every test"""
